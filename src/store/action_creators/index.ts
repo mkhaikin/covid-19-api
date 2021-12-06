e7cfdeaf26d3383b739aaebb691224ad386d4eef
Michael <michael.khaikin@gmail.com>
2021-12-06 15:52:27 -0500

    UPDATE: actionCreators

--- src/store/action_creators/index.ts
+++ src/store/action_creators/index.ts
@@ -1,14 +1,16 @@
-import * as CountryDataCreators from './countrycall'
-import * as HelpDataCreators from './helpcall'
-import * as TotalDataCreators from './totalcall'
-import * as ReportTotalsDataCreators from './reportTotalsCall'
-import * as DailyReportByCountryNameCreators from './dailyReportByCountryNameCall'
+import * as CountryDataCreators from './countrycall';
+import * as HelpDataCreators from './helpcall';
+import * as TotalDataCreators from './totalcall';
+import * as ReportTotalsDataCreators from './reportTotalsCall';
+import * as DailyReportByCountryCodeCreators from './dailyReportByCountryCodeCall';
+import * as DailyReportByCountryNameCreators from './dailyReportByCountryNameCall';
 
 const ActionCreators = {
     ...CountryDataCreators,
     ...HelpDataCreators,
     ...TotalDataCreators,
     ...ReportTotalsDataCreators,
+    ...DailyReportByCountryCodeCreators,
     ...DailyReportByCountryNameCreators,
 }
 export default ActionCreators
